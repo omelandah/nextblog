fa2975bac1d8300e151e64ac420bef2c17dc57d7
anhnt276 <ntanh0393@gmail.com>
2025-03-01 23:49:55 +0700

    init setup BE

---
+++ BE/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || 'localhost';
+
+app.use(express.urlencoded());
+
+app.get('/', (req, res) => {
+  res.send('Hello');
+})
+
+app.listen(port, () => {
+  console.log(`Connected to port ${port}`)
+})
