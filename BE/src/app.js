ab4b6717617eafc37696f107aa2ca8512aa5d17f
anhnt276 <ntanh0393@gmail.com>
2025-03-10 01:06:29 +0700

    add middlewares

--- BE/src/app.js
+++ BE/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 const host = process.env.HOST || 'localhost';
+
+app.use(helmet);
+app.use(cors);
 
 app.use(express.urlencoded());
 
